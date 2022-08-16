f8289c574cf0d6f958d1c71c302bd9227ba370c0
Sridhu-animaker <sridhu@animaker.com>
2022-08-16 20:19:11 +0530

    Styling issue fixed

--- public/main.js
+++ public/main.js
@@ -6,8 +6,8 @@ let win;
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    width: 380,
+    height: 605,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
